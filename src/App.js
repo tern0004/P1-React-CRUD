4c90aecc50bbc92d4a473717431aa141a6bc3415
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-11-03 16:13:01 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
+import MainPage from './containers/MainPage';
+import AddItemPage from './containers/AddItemPage';
+import PageNotFound from './containers/PageNotFound';
 
 
 class App extends Component {
@@ -9,11 +12,9 @@ class App extends Component {
     return (
         <div className="App">
           <Switch>
-            <Route exact path='' component={}/>
-            <Route exact path='' component={}/>
-            <Route exact path='' component={}/>
-            <Route exact path='' component={}/>
-            <Route path='' component={}/>
+            <Route exact path='/' component={MainPage}/>
+            <Route exact path='/add' component={AddItemPage}/>
+            <Route path='/' component={PageNotFound}/>
           </Switch>
         </div>
     );
