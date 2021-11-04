f308e60facd70e7bd237897453d59e69b6cf446a
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-11-04 18:55:02 -0400

    - Implemented add to localStorage in react app

--- src/containers/AddItemPage.js
+++ src/containers/AddItemPage.js
@@ -5,12 +5,34 @@ import ItemElementAPI from "../functions/ItemElementAPI";
 
 class AddItemPage extends Component {
 
-    addToLocalStorage() {
-        let framework = {frameworkName:'Angular', leadBy: 'Google', docs: 'https://angular.io'}
+    state = {
+        frameworkName: null,
+        leadBy: null,
+        docs: null,
+    };
+    addToLocalStorage(name,leadBy,docs) {
+        let framework = {frameworkName: name, leadBy: leadBy, docs: docs}
         ItemElementAPI.addItemElementToLocalStorage(framework);
         console.log(framework)
     }
+    getFrameworkName(event) {
+        this.setState({frameworkName: event.target.value})
+    }
+    getLeadBy(event) {
+        this.setState({leadBy: event.target.value})
+    }
+    getDocs(event) {
+        this.setState({docs: event.target.value})
+    }
+    handleSubmit() {
+        const frameworkName = this.state.frameworkName
+        const leadBy = this.state.leadBy
+        const docs = this.state.docs
+
+        this.addToLocalStorage(frameworkName,leadBy,docs)
+    }
     render() {
+
         return(
             <div>
                 <MainPageHeader/>
@@ -19,13 +41,15 @@ class AddItemPage extends Component {
                 </div>
                 <div>
                     <form>
-                        <TextField id="outlined-basic" label="Framework Name" variant="outlined"/>
-                        <TextField id="outlined-basic" label="Docs URL" variant="outlined"/>
-                        <TextField id="outlined-basic" label="Lead by" variant="outlined"/>
+                        <TextField id="outlined-basic" label="Framework Name" variant="outlined" onChange={(event) => {this.getFrameworkName(event)}}/>
+                        <TextField id="outlined-basic" label="Lead by" variant="outlined" onChange={(event) => {this.getLeadBy(event)}}/>
+                        <TextField id="outlined-basic" label="Docs URL" variant="outlined" onChange={(event) => {this.getDocs(event)}}/>
                     </form>
                 </div>
                 <div>
-                    <Button variant="outlined" color="success" onClick={() => { this.addToLocalStorage()}}>Save</Button>
+                    <Button variant="outlined" color="success" onClick={() => {
+                        this.handleSubmit()
+                    }}>Save</Button>
                     <Button variant="outlined" color="error">Cancel</Button>
                 </div>
             </div>
