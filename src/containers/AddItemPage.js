32c358bc24a6b3c5d94375699cd34ae675b2036a
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-11-04 17:31:43 -0400

    - Init localStorage in react app

--- src/containers/AddItemPage.js
+++ src/containers/AddItemPage.js
@@ -1,8 +1,15 @@
 import React, {Component} from "react";
 import MainPageHeader from "../components/MainPageHeader";
 import {Button, TextField} from "@mui/material";
+import ItemElementAPI from "../functions/ItemElementAPI";
 
 class AddItemPage extends Component {
+
+    addToLocalStorage() {
+        let framework = {frameworkName:'Angular', leadBy: 'Google', docs: 'https://angular.io'}
+        ItemElementAPI.addItemElementToLocalStorage(framework);
+        console.log(framework)
+    }
     render() {
         return(
             <div>
@@ -18,7 +25,7 @@ class AddItemPage extends Component {
                     </form>
                 </div>
                 <div>
-                    <Button variant="outlined" color="success">Save</Button>
+                    <Button variant="outlined" color="success" onClick={() => { this.addToLocalStorage()}}>Save</Button>
                     <Button variant="outlined" color="error">Cancel</Button>
                 </div>
             </div>
---
+++ src/functions/ItemElementAPI.js
@@ -0,0 +1,23 @@
+const ItemElementAPI = {
+
+    addItemElementToLocalStorage: (framework) => {
+        let frameworks
+        if(localStorage.getItem("frameworks") === null) {
+            frameworks = []
+        } else {
+            frameworks = JSON.parse(localStorage.getItem("frameworks"))
+        }
+        frameworks.push(framework)
+        localStorage.setItem("frameworks", JSON.stringify(frameworks))
+    },
+    /*removeItemElementFromLocalStorage:  (framework) => {
+        let frameworks
+        if(localStorage.getItem("frameworks") === null) {
+            frameworks = []
+        } else {
+            //frameworks = JSON.parse(localStorage.getItem("frameworks"))
+        }
+        //let frameworkIndex = frameworks
+    }*/
+}
+export default ItemElementAPI;
