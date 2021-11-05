90bcc97731435ffe9b885b98af601cbeec4a566a
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-11-05 17:57:38 -0400

    - Implemented Add Item in function based react app (DONE)

--- src/components/ItemElement.js
+++ src/components/ItemElement.js
@@ -6,13 +6,13 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {Link} from "@mui/material";
 
-function ItemElement() {
+function ItemElement({frameworkName, leadBy, docs}) {
         return(
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
-                    <Typography variant="h5" component="div"> AAA </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary"> Lead by AAA </Typography>
-                    <Link href="/#" underline="none"> Official Docs </Link>
+                    <Typography variant="h5" component="div"> {frameworkName} </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary"> Lead by {leadBy} </Typography>
+                    <Link href={docs} underline="none"> Official Docs </Link>
                 </CardContent>
                 <CardActions>
                     <Button variant="outlined" color="success" size="small">Edit</Button>
